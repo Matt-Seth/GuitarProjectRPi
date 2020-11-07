3cd17b53512a6ffd1a985cff4b5a918aa4395b05
sethgool <sethggoolsby@gmail.com>
2020-11-07 15:37:45 -0800

    Recording  test

--- usbMic.py
+++ usbMic.py
@@ -1,34 +1,40 @@
 import pyaudio
 import wave
 
-CHUNK = 1024
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 2
+fs = 44100  # Record at 44100 samples per second
+seconds = 3
+filename = "test.wav"
 
-if len(sys.argv) < 2:
-    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
-    sys.exit(-1)
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-wf = wave.open(sys.argv[1], 'rb')
+print('Recording')
 
-# instantiate PyAudio (1)
-p = pyaudio.PyAudio()
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+frames = []  # Initialize array to store frames
 
-# open stream (2)
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
 
-# read data
-data = wf.readframes(CHUNK)
-
-# play stream (3)
-while len(data) > 0:
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-
-# stop stream (4)
+# Stop and close the stream 
 stream.stop_stream()
 stream.close()
-
-# close PyAudio (5)
+# Terminate the PortAudio interface
 p.terminate()
+
+print('Finished recording')
+
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
