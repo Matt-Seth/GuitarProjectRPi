d4cac4bbfd762ea9497b0aefe12be70bfa23e2e2
sethgool <sethggoolsby@gmail.com>
2020-11-07 16:11:04 -0800

    test part 2

--- usbMic.py
+++ usbMic.py
@@ -1,12 +1,13 @@
 import pyaudio
 import wave
 
-chunk = 1024  # Record in chunks of 1024 samples
+chunk = 4096  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 2
+channels = 1
 fs = 44100  # Record at 44100 samples per second
 seconds = 3
 filename = "test.wav"
+dev_index = 0
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
@@ -14,6 +15,7 @@ print('Recording')
 
 stream = p.open(format=sample_format,
                 channels=channels,
+                input_device_index = dev_index,
                 rate=fs,
                 frames_per_buffer=chunk,
                 input=True)
@@ -24,6 +26,7 @@ for i in range(0, int(fs / chunk * seconds)):
     data = stream.read(chunk)
     frames.append(data)
 
+print("finished recording")
 # Stop and close the stream 
 stream.stop_stream()
 stream.close()
