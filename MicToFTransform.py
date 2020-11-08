3e98d473c9ec78e63de19df85d72620d9a45a5bf
sethgool <sethggoolsby@gmail.com>
2020-11-08 15:12:01 -0800

    things

--- MicToFTransform.py
+++ MicToFTransform.py
@@ -4,12 +4,12 @@ import time
 #import thread
 import numpy as np
 
-chunk = 4096  # Record in chunks of 1024 samples
+chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 1
 fs = 44100  # Record at 44100 samples per second
 max_frequeny = fs/2
-secondPerChunk = 1
+secondPerSlice = 1 #each frame to be FFT will be this many seconds long
 filename = "Chunk.wav"
 dev_index = 0
 
@@ -23,16 +23,25 @@ stream = p.open(format=sample_format,
                 input=True)
 
 def getAudioChunk():
+    frames = []
     print("recording")
-    for i in range(0, int(fs / chunk * secondPerChunk)):
+    for i in range(0, int(fs / chunk * secondPerSlice)):
         data = stream.read(chunk)
+        frames.append(data)
     print("done recording")
-    return data
-def fft(data):
-    dataFFT = np.fft.fft(data)
+    return frames
+def fftransform(data):
+    n = int(fs*secondPerFrame)
+    dataFFT = np.fft.fft(data)/n #F transform and normalize by dividing by the expected number of elements in the slice.
+
+
 
 a = getAudioChunk()
 
-print(type(a))
-print(a)
 
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
