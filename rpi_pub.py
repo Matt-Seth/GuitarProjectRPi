8432e627b2989a9b559aec0c1ab4d1dd65eb2b00
sethgool <sethggoolsby@gmail.com>
2020-11-11 21:22:22 -0800

    Putting it together

--- rpi_pub.py
+++ rpi_pub.py
@@ -57,7 +57,6 @@ if __name__ == '__main__':
     Call loop_stop() to stop the background thread. The force argument is currently ignored.
     """
     client.loop_start()
-    mic.mic_init()
     while True:
         #Sensor Readings
         note = ""
